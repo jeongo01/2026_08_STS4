c0f95cccdc039c9510f9398ecb966ff6043f5cef
jeongo01 <wjddh123456@gmail.com>
2026-08-26 16:48:06 +0900

    mybatis setting & ArticleDao(mapper) & DB

--- src/main/java/com/koreaIT/demo/dao/ArticleDao.java
+++ src/main/java/com/koreaIT/demo/dao/ArticleDao.java
@@ -1,61 +1,28 @@
 package com.koreaIT.demo.dao;
 
-import java.util.ArrayList;
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 import com.koreaIT.demo.vo.Article;
 
-public class ArticleDao {
-	private int lastArticleId;
-	private List<Article> articles;
+
+@Mapper
+public interface ArticleDao {
+
+	public Article writeArticle(String title, String body);
+
+	@Select("""
+			SELECT *
+			    FROM Article
+			    WHERE id = #{id}
+			""")
+	public Article getArticleById(int id);
+
+	public void modifyArticle(Article article, String title, String body);
 	
-	public ArticleDao() {
-		this.lastArticleId = 0;
-		this.articles = new ArrayList<>();
+	public void deleteArticle(Article article);
 
-		makeTestData();
-	}
-
-	private void makeTestData() {
-		for (int i = 0; i < 10; i++) {
-
-			String title = "제목" + i;
-			String body = "내용" + i;
-
-			writeArticle(title, body);
-		}
-	}
-
-	public Article writeArticle(String title, String body) {
-
-		lastArticleId++;
-
-		Article article = new Article(lastArticleId, title, body);
-		articles.add(article);
-
-		return article;
-	}
-
-	public Article getArticleById(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
-	}
-
-	public void modifyArticle(Article article, String title, String body) {
-		article.setTitle(title);
-		article.setBody(body);
-	}
-
-	
-	public void deleteArticle(Article article) {
-		articles.remove(article);
-	}
-
-	public List<Article> getArticles() {
-		return articles;
-	}
+	public List<Article> getArticles();
 }
