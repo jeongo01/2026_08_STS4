6a4852397bb50bab96528a435d777b699674bfeb
jeongo01 <wjddh123456@gmail.com>
2026-08-30 18:54:19 +0900

    ResultData

--- src/main/java/com/koreaIT/demo/controller/UsrMemberController.java
+++ src/main/java/com/koreaIT/demo/controller/UsrMemberController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.koreaIT.demo.service.MemberService;
 import com.koreaIT.demo.util.Util;
 import com.koreaIT.demo.vo.Member;
+import com.koreaIT.demo.vo.ResultData;
 
 @Controller
 public class UsrMemberController {
@@ -18,42 +19,44 @@ public class UsrMemberController {
 	
 	@RequestMapping("usr/member/join")
 	@ResponseBody
-	public Object doJoin(String loginId, String loginPw, String nickname, String name, String cellphoneNum, String email) {
+	public ResultData doJoin(String loginId, String loginPw, String nickname, String name, String cellphoneNum, String email) {
 		
 		if(Util.empty(loginId)) {
-			return "다시 입력하시오.";
+			return ResultData.from("F-1", "아이디를 입력해주세요.");
 		}
 		
 		if(Util.empty(loginPw)) {
-			return "다시 입력하시오.";
+			return ResultData.from("F-2", "비밀번호를 입력해주세요.");
 		}
 		
 		if(Util.empty(nickname)) {
-			return "다시 입력하시오.";
+			return ResultData.from("F-3", "닉네임을 입력해주세요.");
 		}
 		
 		if(Util.empty(name)) {
-			return "다시 입력하시오.";
+			return ResultData.from("F-4", "이름을 입력해주세요.");
 		}
 		
 		if(Util.empty(cellphoneNum)) {
-			return "다시 입력하시오.";
+			return ResultData.from("F-5", "전화번호를 입력해주세요.");
 		}
 		
 		if(Util.empty(email)) {
-			return "다시 입력하시오.";
+			return ResultData.from("F-6", "이메일을 입력해주세요.");
 		}
 		
 		Member member = memberService.getMemberByLoginId(loginId);
 		
 		if(member != null) {
-			return loginId + "은(는) 현재 사용중인 아이디입니다.";
+			return ResultData.from("F-A", loginId + "은(는) 현재 사용중인 아이디입니다.");
 		}
 		
 		memberService.joinMember(loginId, loginPw, nickname, name, cellphoneNum, email);
 		
+		
 		int id = memberService.getLastInsertId();
 		
-		return memberService.getMemberById(id);
+		
+		return ResultData.from("S-1", "회원가입 성공", memberService.getMemberById(id));
 	}
 }
